cb8da85c8f32a411ae8e2e490de9ab51181cb711
agnieszka zambrzycka <agnieszka.zambrzycka@partner.bmw.de>
2025-01-15 19:42:30 +0100

    Remove old files

--- src/app/hello.py
+++
@@ -1,6 +0,0 @@
-def hello(name="dupa"):
-    return f"Rise and shine {name}"
-
-
-if __name__ == "__main__":
-    print(hello())
--- src/app/set_database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-from definitions import DATABASE_PATH
-
-if not DATABASE_PATH.exists():
-    conn = sqlite3.connect(DATABASE_PATH)
-    conn.close()
