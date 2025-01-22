b579e13594661a8aaf69d0d611823185618a07c5
Piotr Ludewicz <piotr.ludewicz@gmail.com>
2025-01-22 16:54:07 +0100

    Initial App. Tested, Woring

--- src/app/routers/plant.py
+++ src/app/routers/plant.py
@@ -8,4 +8,4 @@ router = APIRouter(responses={404: {"description": "Plant not found"}})
 @router.get("/plant/{name}", response_model=PlantResponse)
 def get_plant(name: str):
     repo = PlantRepository()
-    return repo.get(name=name)
+    return repo.get_by_name(name=name)
